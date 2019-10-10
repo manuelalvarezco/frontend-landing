cfd610cc9f4642e72c5562983aa745745c1d5508
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2019-10-10 11:56:43 -0500

    Separando componentes y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { CarruselComponent } from './componets/carrusel/carrusel.component';
+import { FormularioComponent } from './componets/formulario/formulario.component';
+import { MainComponent } from './componets/main/main.component';
+import { SectionOneComponent } from './componets/section-one/section-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarruselComponent,
+    FormularioComponent,
+    MainComponent,
+    SectionOneComponent
   ],
   imports: [
     BrowserModule
